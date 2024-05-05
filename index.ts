e8df68d042a396ef5a261dc67d43b040c5811d80
Leviathenn <levixanderson@hotmail.com>
2024-05-04 20:03:44 -0700

    updated sources

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import { setTimeout } from "node:timers/promises"
-import { Axios } from "axios";
+import axios, { Axios } from "axios";
 import { Spinner } from "@topcli/spinner";
 import crypto from "node:crypto"
 import { temp } from "./tools/temp.ts";
 import { time, timeEnd } from "node:console"
+import { writeFileSync } from "node:fs";
 
 let APP_VERSION = "0.1.0"
-
+let APP_RAW = "https://raw.githubusercontent.com/Leviathenn/upatcher/main"
 time();
 
 var getHash = ( content: string ) => {				
@@ -28,15 +29,23 @@ console.log(`
     BUILT AND COMPILED WITH DENO
     SOURCES FILE HOSTED ON GITHUB FOR CUSTOM SOURCES SEE GUIDE.
     WRITIN IN TYPESCRIPT
-    LICENSE IS MIT BY USING YOU ACCEPT TO THESE TERMS. ENJOY!
-    
+    LICENSE IS MIT BY USING YOU ACCEPT TO THESE TERMS. ENJOY! 
 
 `)
 //await setTimeout(1000);
 
-//const lSpinner = new Spinner().start("Retriving Sources ");
+const lSpinner = new Spinner().start("Retriving Sources.. "); 
 
 let tempd = new temp();
+let sourcesFile: string = "";
+axios.get(`${APP_RAW}/sources.json`).then((res)=>{
+    sourcesFile = tempd.write(JSON.stringify(res["data"]));
+    lSpinner.succeed("Retrived Sources.");
+    console.log(sourcesFile);
+}).catch((err)=>{
+    lSpinner.failed("Could not Retrive sources. Check the \"error.log\" file for more info.");
+    writeFileSync("error.log",err.toString());
+})
 
 //let src: source = new source("sources.list");
 //src.get();
--- tools/temp.ts
+++ tools/temp.ts
@@ -1,23 +1,45 @@
 /**
  * @author Leviathenn
  */
-import {type PathLike} from "node:fs"
+import {existsSync, mkdirSync, readFile, readFileSync, rmSync, writeFileSync, type PathLike} from "node:fs"
+import { tmpdir } from "node:os";
+
 export class temp{
-    private tempFolder: PathLike;
+    private tempFolder!: PathLike;
     
+    write(content: string): string{
+        let gr = this.generateString(28);
+        if(existsSync(`${this.tempFolder}/${gr}`)){
+            gr = this.generateString(29);
+        }else{
+            writeFileSync(`${this.tempFolder}/${gr}`,content);
+        }
+        return gr;
+    }
+    read(key: string): Buffer{
+        return readFileSync(`${this.tempFolder}/${key}`);
+    }
+    private generateString(length: number){
+        const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = ' ';
+        const charactersLength = characters.length;
+        for ( let i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+    
+        return result;
+    }
     constructor(){
         
-        function generateString(length: number) {
-            const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-            let result = ' ';
-            const charactersLength = characters.length;
-            for ( let i = 0; i < length; i++ ) {
-                result += characters.charAt(Math.floor(Math.random() * charactersLength));
-            }
         
-            return result;
+        let fP: string = this.generateString(28)
+        let tmpDir = tmpdir();
+        if(existsSync(`${tmpDir}/${fP}`)){
+            rmSync(`${tmpDir}/${fP}`,{recursive: true, force: true});
+            this.tempFolder = `${tmpdir}/${fP}`;
+        }else{
+            mkdirSync(`${tmpDir}/${fP}`);
+            this.tempFolder = `${tmpDir}/${fP}`;
         }
-        let fP: string = generateString(28)
-        console.log(fP);
     }
 }
